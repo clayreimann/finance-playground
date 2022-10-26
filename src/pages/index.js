d0c5e3eeb4ef84c9a8dead7bc31f7c068d235961
Clay Jensen-Reimann <clayreimann@gmail.com>
2022-10-26 12:35:18 -0500

    Add github actions (#2)
    * Add github actions
    * Add test script

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/category/loans">
+            to="/docs/category/loans">
             Compare Loans
           </Link>
         </div>
